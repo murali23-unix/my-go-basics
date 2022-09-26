d0a1afde49750862b271da69e2aec6699c0531d5
Muralidharan V Krishnakumar <murali23.unix@gmail.com>
2022-09-26 14:43:38 +0530

    adding comment lines

--- my-basics.go
+++ my-basics.go
@@ -4,5 +4,17 @@ import "fmt"
 
 func main() {
 
-	fmt.Printf("My Go Basics Program starts here")
+	fmt.Printf("\nMy Go Basics Program starts here\n")
+
+	// This is a single line comment
+	/* This is a multiple line comment
+	we can use this method */
+
+	fmt.Printf("\nLets look into variables now\n")
+
+	var name = "SriGanesh"
+	var age = 10
+
+	fmt.Println(name, age)
+
 }
